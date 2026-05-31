f2ce810e49a816a220b75cdc71f875a22507b984
world-pc <287334486+world-pc@users.noreply.github.com>
2026-05-31 00:51:27 -0400

    renamed this file.

--- digit-recognition.py
+++
@@ -1,9 +0,0 @@
-from keras.datasets import mnist
-import numpy as np
-
-#load in the training and testing data from MNIST
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-#create a matrix of weights for each neuron (10 neurons in this case)
-W = np.random.randn(10, 784) * 0.01
-
